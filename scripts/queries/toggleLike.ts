062c57197a3cd38d7f87fd7f77fca38da0509ef2
Philip <pvu2002@outlook.com>
2023-12-10 01:06:57 -0800

    feat: Refactor toggle queries to use onConflictDoNothing

--- scripts/queries/toggleLike.ts
+++ scripts/queries/toggleLike.ts
@@ -1,23 +1,12 @@
 import { db } from '$lib/db';
-import { song, user, userLikes } from '$lib/db/schema';
-import { and, eq, sql } from 'drizzle-orm';
+import { userLikes } from '$lib/db/schema';
+import { and, eq } from 'drizzle-orm';
 
 export const toggleLike = (userId: string, songId: string, like: boolean) =>
 	db.transaction(async (tx) => {
-		const isLiked =
-			(
-				await tx
-					.select({ count: sql<number>`COUNT(*)` })
-					.from(userLikes)
-					.innerJoin(user, eq(userLikes.userId, userId))
-					.innerJoin(song, eq(userLikes.songId, songId))
-			)[0].count > 0;
-
-		if (like === isLiked) return like;
-
 		if (like) {
-			await tx.insert(userLikes).values({ userId, songId });
-		} else if (!like) {
+			await tx.insert(userLikes).values({ userId, songId }).onConflictDoNothing();
+		} else {
 			await tx
 				.delete(userLikes)
 				.where(and(eq(userLikes.songId, songId), eq(userLikes.userId, userId)));
--- scripts/queries/togglePlaylistSong.ts
+++ scripts/queries/togglePlaylistSong.ts
@@ -4,28 +4,19 @@ import { and, eq, sql } from 'drizzle-orm';
 
 export const togglePlaylistSong = (playlistId: string, songId: string, method: 'ADD' | 'REMOVE') =>
 	db.transaction(async (tx) => {
-		const [playlistSongExists, latestOrder] = await Promise.all([
-			(async () =>
-				(
-					await tx
-						.select({ count: sql<number>`COUNT(*)` })
-						.from(playlistSongs)
-						.where(and(eq(playlistSongs.playlistId, playlistId), eq(playlistSongs.songId, songId)))
-						.limit(1)
-				).length === 0)(),
-			(async () =>
+		if (method === 'ADD') {
+			const latestOrder =
 				(
 					await tx
 						.select({ order: sql<number>`MAX(${playlistSongs.order})` })
 						.from(playlistSongs)
 						.where(eq(playlistSongs.playlistId, playlistId))
-				)[0].order || 0)()
-		]);
-
-		if (playlistSongExists && method === 'ADD') return true;
-
-		if (method === 'ADD') {
-			await tx.insert(playlistSongs).values({ playlistId, songId, order: latestOrder + 1 });
+						.limit(1)
+				).at(0)?.order ?? 0;
+			await tx
+				.insert(playlistSongs)
+				.values({ playlistId, songId, order: latestOrder + 1 })
+				.onConflictDoNothing();
 			return true;
 		} else {
 			await tx
